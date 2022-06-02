f4a83f160c14e9faf89c91c41392bee80b108579
Bram-Wel <welbram@gmail.com>
2022-06-02 15:19:49 +0000

    Stuff.

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+
+def abs_no(n):
+    """Return the absolute value of number."""
+    if n < 0:
+        return -n
+    return n
+
+
+num = abs_no(number)
+last_digit = num % 10
+str0 = f"Last digit of {number} is {last_digit} and is "
+if last_digit > 5:
+    str1 = f"greater than 5"
+elif last_digit == 0:
+    str1 = f"0"
+else:
+    str1 = f"less than 6 and not 0"
+print(str0 + str1)
