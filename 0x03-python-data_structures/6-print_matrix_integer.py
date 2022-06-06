3872904b2ff3ba6b4c5e596b4ae3bddd3e20e4ea
Bram-Wel <welbram@gmail.com>
2022-06-06 16:11:38 +0000

    Structures => No comma.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,5 +10,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix[0]:
         for i in matrix:
             for j in range(len(i) - 1):
-                print("{:d}".format(i[j]), end=", ")
+                print("{:d}".format(i[j]), end=" ")
             print("{:d}".format(i[len(i) - 1]))
+    else:
+        print()
