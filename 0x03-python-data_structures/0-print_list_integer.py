3c60b810dfb5a68b765d1ed72c57b030f44ae79d
Bram-Wel <welbram@gmail.com>
2022-06-06 13:29:33 +0000

    Structures.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -8,5 +8,7 @@ def print_list_integer(my_list=[]):
     """
 
     for i in my_list:
-        if type(i) == int:
-            print("{:d}".format(i))
+        """if type(i) == int:
+        """
+
+        print("{:d}".format(i))
