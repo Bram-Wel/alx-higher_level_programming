bf93469e37e21972a4b92c97acb5dd66d997927d
Bram-Wel <welbram@gmail.com>
2022-06-06 16:05:29 +0000

    Structures.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -12,5 +12,3 @@ def print_matrix_integer(matrix=[[]]):
             for j in range(len(i) - 1):
                 print("{:d}".format(i[j]), end=", ")
             print("{:d}".format(i[len(i) - 1]))
-    else:
-        print()
