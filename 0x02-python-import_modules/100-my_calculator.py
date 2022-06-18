de98b4e4ab7478fd5d3557d4b2f14db4531c1077
Bram-Wel <welbram@gmail.com>
2022-06-18 18:57:47 +0000

    Simple Calc.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,7 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def calculate():
     """Handles basic Calculator operations
     """
@@ -14,13 +15,18 @@ def calculate():
         print(f"Unknown Operator. Available operators: +, -, * and /")
         exit(1)
     elif sys.argv[2] == '+':
-            print(f"{sys.argv[1]} + {sys.argv[3]} = {add(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{sys.argv[1]} + {sys.argv[3]} = ", end="")
+        print(f"{add(int(sys.argv[1]), int(sys.argv[3]))}")
     elif sys.argv[2] == '-':
-            print(f"{sys.argv[1]} - {sys.argv[3]} = {sub(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{sys.argv[1]} - {sys.argv[3]} = ", end="")
+        print(f"{sub(int(sys.argv[1]), int(sys.argv[3]))}")
     elif sys.argv[2] == '*':
-            print(f"{sys.argv[1]} * {sys.argv[3]} = {mul(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{sys.argv[1]} * {sys.argv[3]} = ", end="")
+        print(f"{mul(int(sys.argv[1]), int(sys.argv[3]))}")
     elif sys.argv[2] == '/':
-            print(f"{sys.argv[1]} / {sys.argv[3]} = {div(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{sys.argv[1]} / {sys.argv[3]} = ", end="")
+        print(f"{div(int(sys.argv[1]), int(sys.argv[3]))}")
+
 
 if __name__ == "__main__":
     calculate()
