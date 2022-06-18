8927f0b52922a8d5badba8877ce858ce0beeafb0
Bram-Wel <welbram@gmail.com>
2022-06-18 18:47:38 +0000

    Simple Calc.

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, mul, div
+
+def calculate():
+    """Handles basic Calculator operations
+    """
+
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        sys.exit(1)
+    elif sys.argv[2] not in "+-*/":
+        print(f"Unknown Operator. Available operators: +, -, * and /")
+        exit(1)
+    elif sys.argv[2] == '+':
+            print(f"{sys.argv[1]} + {sys.argv[3]} = {add(int(sys.argv[1]), int(sys.argv[3]))}")
+    elif sys.argv[2] == '-':
+            print(f"{sys.argv[1]} - {sys.argv[3]} = {sub(int(sys.argv[1]), int(sys.argv[3]))}")
+    elif sys.argv[2] == '*':
+            print(f"{sys.argv[1]} * {sys.argv[3]} = {mul(int(sys.argv[1]), int(sys.argv[3]))}")
+    elif sys.argv[2] == '/':
+            print(f"{sys.argv[1]} / {sys.argv[3]} = {div(int(sys.argv[1]), int(sys.argv[3]))}")
+
+if __name__ == "__main__":
+    calculate()
