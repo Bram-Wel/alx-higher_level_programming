b35c1abdf9a7ce6209895b0e47e32cde39fd766a
Bram-Wel <welbram@gmail.com>
2022-06-18 20:39:18 +0000

    ByteCode.

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calc(a, b):
+    if a < b:
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(i, c)
+
+        return c
+    else:
+        return sub(a, b)
