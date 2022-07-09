fbc82b75b100b7a917fd7da1d09aad10ada56e9a
Bram-Wel <welbram@gmail.com>
2022-07-09 14:33:17 +0000

    Python Exceptions.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,9 +12,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for el in range(x):
             print(my_list[el], end="")
-        if (x > 0):
-            el += 1
-        return (el)
+        if (x == 0):
+            return (0)
+        return (el + 1)
     except IndexError:
         return (el)
     finally:
