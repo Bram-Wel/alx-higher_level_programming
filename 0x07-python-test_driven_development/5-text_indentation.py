d98518ae8481ce586f02f16d6b47c0076ab65d91
Bram-Wel <welbram@gmail.com>
2022-09-22 10:46:34 +0000

    Test Driven Dvpt.

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This describes a function that prints formated text"""
+
+
+def text_indentation(text):
+    """Prints text in an indented format
+
+    Args:
+        text: Text to b printed
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        a = 0
+        for i in range(len(text)):
+            if text[i] in '.?:':
+                print(text[a:i + 1])
+                print()
+                a = i + 2
+                state = True
+        if i + 1 == len(text) and state is False:
+            print(text)
