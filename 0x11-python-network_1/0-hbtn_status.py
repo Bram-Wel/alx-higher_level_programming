46055d1348aa816a7011a3bea9b32891cbf9fca7
Bram-Wel <welbram@gmail.com>
2022-09-30 09:19:20 +0000

    fetch with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 if __name__ == "__main__":
     with urlopen('https://alx-intranet.hbtn.io/status') as resp:
         body = resp.read()
-        print("Body Response:")
+        print("Body response:")
         print(f"\t- type: {type(body)}")
         print(f"\t- content: {body}")
         print(f"\t- utf8 content: {body.decode('utf-8')}")
