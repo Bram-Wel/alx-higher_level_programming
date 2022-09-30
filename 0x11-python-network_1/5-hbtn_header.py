8de08efd9354f6fd45e3107769f671a14bf8a7ba
Bram-Wel <welbram@gmail.com>
2022-09-30 12:58:32 +0000

    requests Package-> Headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Fetches a url{requests package} and displays a header"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    resp = requests.get(argv[1])
+    print(resp.headers['X-Request-Id'])
