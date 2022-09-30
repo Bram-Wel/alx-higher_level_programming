d93115ae3d11fbdd0d8fef7028df141d90c61481
Bram-Wel <welbram@gmail.com>
2022-09-30 13:51:55 +0000

    requests Package-> Headers

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,5 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    resp = requests.get(argv[1])
-    print(resp.headers.get('X-Request-Id')
+    resp = requests.post(argv[1])
+    print(resp.headers['X-Request-Id'])
