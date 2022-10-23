55b24cdebb16b7b6d36f2a9f1e09ee56207717cf
Bram-Wel <welbram@gmail.com>
2022-10-23 19:49:09 +0000

    type() & isinstance()

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The module defines an evaluating function """
+
+def is_same_class(obj, a_class):
+    """Evaluates if an object is an instance of a class
+
+    Args:
+        obj: Object to evaluate
+        a_class: Susspect class
+    """
+    return isinstance(obj, a_class)
