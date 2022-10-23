40f8734e35e15f2ebeb0bbc43532e23a038f7a3a
Bram-Wel <welbram@gmail.com>
2022-10-23 20:45:18 +0000

    base class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a Class"""
+
+
+class BaseGeometry:
+    """A Base Class"""
+
+    def __init__(self):
+        """Initialisation Function"""
+        pass
+
+    def area(self):
+        """Geomatrical area of object/surface"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validates @value"""
+        if not isinstance(value, int):
+            raise TypeError(name + ' must be an integer')
+        elif value < 1:
+            raise ValueError(name + ' must be greater than 0')
