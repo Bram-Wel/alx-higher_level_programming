5848445aea20492d1769105251e5eb0067ff7ce1
Bram-Wel <welbram@gmail.com>
2022-10-23 14:01:18 +0000

    Using dir bult in functions

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module describes the looup() function"""
+
+
+def lookup(obj):
+    """Find the attributes and methods of a python object
+
+    Args:
+        obj: Python object
+    """
+    return dir(obj)
