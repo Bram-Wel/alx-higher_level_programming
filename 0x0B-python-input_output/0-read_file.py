b3075cdaf6f993c278ad299127116f5818e839b0
Bram-Wel <welbram@gmail.com>
2022-11-06 10:12:39 +0300

    Python I/O + Automation

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module describes a file reading function."""
+
+
+def read_file(filename=""):
+    """Read a text file, using encoding utf-8 then print contents to stdout.
+
+    Args:
+        @filename: Name of file to read
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read())
+
+
+if __name__ == "__main__":
+    pass
