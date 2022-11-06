1c83d91c838de48325e315ee7157e035e2f555da
Bram-Wel <welbram@gmail.com>
2022-11-06 11:35:37 +0300

    Python I/O + Automation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""This module describes a json serialization function."""
+"""This module describes a json deserialization function."""
 
 
 import json
 
-def to_json_string(my_obj):
-    """Encode an object in json format.
+
+def from_json_string(my_str):
+    """Decode an object from json string.
 
     Args:
-        my_obj: Object to serialize/encode
+        my_str: Json string to deserialize/decode
     Return:
-        Encoded json string of the object
+        Decoded python object
     """
-    return json.dumps(my_obj)
+    return json.loads(my_str)
