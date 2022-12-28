fc1c820efc6731515351c2d8fe6919fa9c20001f
Bram-Wel <welbram@gmail.com>
2022-12-28 17:12:19 +0300

    Json String.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,7 +47,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Return json string representing list_dictionaries."""
         if list_dictionaries is None or list_dictionaries == []:
-            rt = "[]"
-        else:
-            rt = json.dumps(list_dictionaries)
-        return rt
+            return "[]"
+        # else:
+        return json.dumps(list_dictionaries)
+        # return rt
