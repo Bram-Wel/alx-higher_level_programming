2751d02f7852fd271f92d0a82471da3188bc75a8
Bram-Wel <welbram@gmail.com>
2023-01-24 00:14:32 +0300

    Select all rows, where clause Order descending

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- List ordered Records of score >= 10
+-- Select score, name from second_table where score >= 10
+-- Order by Descending
+SELECT score, name
+        FROM second_table
+        WHERE score >= 10
+        ORDER BY score DESC;
