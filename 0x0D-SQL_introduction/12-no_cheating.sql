cbf2b9e1892a93ddc34f9efbc2da3763f480f5f1
Bram-Wel <welbram@gmail.com>
2023-01-24 00:55:32 +0300

    Update row

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Update Bob's score to 10 in second_table
+-- Update score to 10 in second_table 2nd table
+-- for record with name Bob
+UPDATE second_table
+        SET score = 10
+        WHERE name = "Bob";
