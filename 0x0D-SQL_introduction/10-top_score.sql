76aeaccf0679dd20d777fed240e7bab33f7f8fc2
Bram-Wel <welbram@gmail.com>
2023-01-24 00:05:00 +0300

    Select all rows, Order descending

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List all records in second_table.
+-- Select all score, name records from second_table and order
+-- resulting table by Descending scores
+SELECT score, name
+        FROM second_table
+        ORDER BY score DESC;
