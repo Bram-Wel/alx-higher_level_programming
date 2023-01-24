edcbc5aa688e3815d85b29917e006184b6f67152
Bram-Wel <welbram@gmail.com>
2023-01-24 23:53:34 +0300

    create table

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,9 @@
+-- Create table
+-- @force_name: Table name
+-- Att:
+--  id (INT): Record id
+--  name (VARCHAR): String value
+-- Create force_name if not exists
+CREATE TABLE IF NOT EXISTS force_name (
+        id INT, name VARCHAR(256)
+);
