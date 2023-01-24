960df740c4acf1da6f52a51dc0cfdff8fc27bc96
Bram-Wel <welbram@gmail.com>
2023-01-24 23:20:54 +0300

    Show Grants

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List mysql user privileges
+-- Show grant privileges for user_0d_1 and user_0d_2 on
+-- localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
