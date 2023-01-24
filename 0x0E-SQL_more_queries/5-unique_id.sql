d8e1bf2abb45b8e174da9d947056e85184abfd2c
Bram-Wel <welbram@gmail.com>
2023-01-25 00:15:22 +0300

    Unique

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Create unique_id table
+-- @id (INT): DEFAULT=1 UNIQUE
+-- @name (VARCHAR): String value
+CREATE TABLE IF NOT EXISTS unique_id (
+        id INT UNIQUE DEFAULT 1,
+        name VARCHAR(256)
+);
