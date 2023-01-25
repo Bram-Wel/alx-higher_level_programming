ae93cdfd37e7ae6a6951f6d74679cfff71a96c26
Bram-Wel <welbram@gmail.com>
2023-01-25 14:12:48 +0300

    NOT IN: Subquery

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,14 @@
+-- List all genres of the show Dexter
+-- Select name from tv_genres not in genres linked to
+-- tv_show with title "Dexter"
+SELECT name
+        FROM tv_genres
+        WHERE name NOT IN
+        (SELECT name
+        FROM tv_genres
+        INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+        INNER JOIN tv_shows
+        ON tv_show_genres.show_id = tv_shows.id
+        WHERE title = "Dexter")
+        ORDER BY name;
