5a6d98ac3742c33ede164ee39c64e500160ca182
Bram-Wel <welbram@gmail.com>
2023-01-25 12:18:43 +0300

    AS keyword

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- Select name, count show_id from tv_genres, natural join
 -- tv_show_genres in groups by genre names sorted in descending
 -- no. of shows
-SELECT name, COUNT(show_id) AS number_of_shows
+SELECT name AS genre, COUNT(show_id) AS number_of_shows
         FROM tv_genres
         INNER JOIN tv_show_genres
         ON id = genre_id
