835f7bb658293fb457db468ebb02714e2d31d6f8
Bram-Wel <welbram@gmail.com>
2023-01-25 11:50:03 +0300

    Inner Join: tv_genres, tv_show_genres counting tv_shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- List all genres in DB and the no. of linked shows
+-- Select name, count show_id from tv_genres, natural join
+-- tv_show_genres in groups by genre names sorted in descending
+-- no. of shows
+SELECT name, COUNT(show_id) AS number_of_shows
+        FROM tv_genres
+        INNER JOIN tv_show_genres
+        ON id = genre_id
+        GROUP BY name
+        ORDER BY number_of_shows DESC;
