31aa6efc792663fe4b1d796ae5c01c9b865f3cf6
Bram-Wel <welbram@gmail.com>
2023-02-22 02:46:06 +0300

    List all states from database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""List all states from database {hbtn_0e_0_usa}."""
+
+import MySQLdb
+
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
+        cursor = db.cursor()
+        cursor.execute("""SELECT * FROM hbtn_0e_0_usa.states""")
+    except MySQLdb.Error as e:
+        print(e)
+    else:
+        row = cursor.fetchone()
+        while row:
+            print(row)
+            row = cursor.fetchone()
+        cursor.close()
+        db.close()
