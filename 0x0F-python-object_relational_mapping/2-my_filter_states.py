594ba6393a21dc213e4ebb8a9b788acc7d9896bc
Bram-Wel <welbram@gmail.com>
2023-02-22 11:53:48 +0300

    SQL Literals

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List all states from database {hbtn_0e_0_usa}."""
+
+import MySQLdb
+
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
+        cursor = db.cursor()
+        cursor.execute("""SELECT * FROM {}.states WHERE name = %s
+                        ORDER BY id ASC""".format(argv[3]), (argv[4],))
+    except MySQLdb.Error as e:
+        print(e)
+    else:
+        row = cursor.fetchone()
+        while row:
+            print(row)
+            row = cursor.fetchone()
+        cursor.close()
+        db.close()
