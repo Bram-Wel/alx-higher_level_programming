f778f5e4ca2ff79e08a72e048f4e220edac710d2
Bram-Wel <welbram@gmail.com>
2023-02-22 13:46:37 +0300

    JOIN, SQLiteral Strings & SQLInjection

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List all states from database {hbtn_0e_0_usa}."""
+
+import MySQLdb
+
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
+        cursor = db.cursor()
+        cursor.execute("""SELECT cities.name FROM cities INNER JOIN states ON
+                        cities.state_id = states.id WHERE states.name = %s
+                        ORDER BY cities.id ASC""", (argv[4],))
+    except MySQLdb.Error as e:
+        print(e)
+    else:
+        row = cursor.fetchone()
+        cities = []
+        while row:
+            cities.append(row[0])
+            row = cursor.fetchone()
+        print(', '.join(cities))
+        cursor.close()
+        db.close()
