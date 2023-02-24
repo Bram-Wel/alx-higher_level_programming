9af4954d0fbc3dabbf7e3d1e47ded00ef25b1769
Bram-Wel <welbram@gmail.com>
2023-02-25 01:28:48 +0300

    Filter by column

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List all State objects from a the database."""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import Session
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/\
+{argv[3]}', pool_pre_ping=True)
+    # Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    query_state = session.query(State).filter(
+        State.name == argv[4]).order_by(State.id).first()
+    if query_state:
+        print(f"{query_state.id}")
+    else:
+        print("Not found")
+    session.close()
